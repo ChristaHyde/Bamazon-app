6d47b5f9341df174b6126b88fc8f3bcc00e2699c
Christa <christahyde@yahoo.com>
2019-04-24 10:24:55 -0600

    I finished

---
+++ bamazonCustomer.js
@@ -0,0 +1,97 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'bamazon_db'
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('Connected to DB');
+    start();
+});
+
+function start() {
+    getAllItems()
+}
+
+function getItemById() {
+    inquirer.prompt([{
+        name: 'id',
+        type: 'input',
+        message: 'give me a product id (ctrl c to exit)',
+    }]).then(function (answer) {
+        console.log(answer);
+        connection.query('SELECT * FROM products WHERE item_id = ?', [answer.id], function (error, result) {
+            if (error) throw error;
+            console.table(result);
+            // get quantity function below
+            inquirer.prompt([{
+                name: 'quantity',
+                type: 'input',
+                message: 'what is the quantity you want? (ctrl c to exit)',
+            }]).then(function (answer) {
+                console.log(answer.quantity);
+                var userQuantity = answer.quantity;
+                var stockQuantity = result[0].stock_quantity;
+                console.log(result[0].stock_quantity);
+                if (userQuantity > stockQuantity) {
+                    console.log("insufficent stock");
+                    start()
+                } else {
+                    console.log(stockQuantity - userQuantity);
+                    // make the query to the database
+                    // use the same id as before
+                    // use that id to select that item again 
+                    // use the equation from line 46 to update the new stock level
+                }
+
+
+            })
+
+        })
+    })
+}
+
+function getAllItems() {
+    connection.query('SELECT * from products', function (error, results) {
+        if (error) throw error;
+        console.table(results);
+        getItemById()
+    });
+}
+
+
+
+
+
+
+
+
+
+
+function searchByArtist(artist) {
+    var query = 'select * from top1000 where artist = ?';
+    connection.query(query, [artist], function (err, data) {
+        if (err) throw err;
+        for (var x = 0; x < data.length; x++) {
+            console.log(data[x].artist, data[x].song, data[x].release_year);
+        }
+        start();
+    });
+}
+
+function searchByYear(year) {
+    var query = 'select * from top1000 where release_year = ?';
+    connection.query(query, [year], function (err, data) {
+        if (err) throw err;
+        for (var x = 0; x < data.length; x++) {
+            console.log(data[x].artist, data[x].song, data[x].release_year);
+        }
+        start();
+    });
+}
---
+++ bamazonDB.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE database bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE top5000 (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  song VARCHAR(100) NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
